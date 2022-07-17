16530e61e7145566a100b4bf20ca9b41e74db771
nmttuan <nmttuan17@gmail.com>
2022-07-17 13:43:21 +0700

    Remove graphs of previously added combinations

--- pages/analytics.py
+++ pages/analytics.py
@@ -4,65 +4,41 @@ import util
 from data import *
 from dash.exceptions import PreventUpdate
 import plotly.express as px
+
+from util.func import bang_diem
 dash.register_page(__name__, path_template="/analytics/<sbd>")
 
 
 def layout(sbd=0, **custom):
-    custom = util.remove_redundant_queries(custom)
     if sbd:
-        d = util.tra_diem(data, diem, sbd)
-
-        # Create graphs for every subjects
-        all_graphs = []
-        personal_scores = diem[d.index] # Remove NaN scores
-
-        for col in personal_scores.columns:
-            data_to_plot = personal_scores[col].value_counts().sort_index()
-            fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
-            fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
-
-            info = html.Div(className='info', children = 
-                        [html.H6(f'Điểm của bạn: {d[col]:.2f}'),
-                        html.H6(f'''Điểm của bạn cao hơn {util.percentile(data=diem[col], value = d[col])} các thí sinh khác!
-                         Chỉ có {util.higher(data=diem[col], value = d[col])} thí sinh có điểm cao hơn bạn!'''),
-                        ])
-
-            all_graphs.append(
-                html.Div(className = 'fig_container' ,children=[
-                    dcc.Graph(figure=fig, className='graph'),
-                    info,
-                    ]))
-
-        # Buttons to create custom combinations of subjects
-
-        ## Combination container
+        custom = util.remove_redundant_queries(custom)
+    
+            # Create graphs for every subjects
+        all_graphs = util.create_graphs(sbd)
+            # Buttons to create custom combinations of subjects
+    
+            ## Combination container
         sj_container = html.Div(id='sj_container', children=[])
-
-        
-        ## Add a subject to the combination
+    
+            
+            ## Add a subject to the combination
         add_button = html.Button('Add', id='add-subject')
-        ## Press this button to submit the custom combination
+            ## Press this button to submit the custom combination
         submit = html.Button('Submit', id='submit-combination')
         res_submit = html.Div(id='res-submit')
-        
-
-
-        # Provide a custom graph if query string is provided
+            
+        bang_diem(sbd)
+    
+            # Provide a custom graph if query string is provided
         if custom:
             all_graphs.insert(0, util.custom_combi(custom, sbd))
-
+    
         return html.Div(
                         children = [
                             sj_container,
                             add_button, submit,
                             res_submit,
                             html.H4(children = f'Số báo danh: {sbd}', style={'text-align': 'center'}),
-                            html.Table(
-                                children=[
-                                    html.Tr(className='subjects', children=
-                                        [html.Th(className = 'subject', style={'text-align': 'center', 'border': '1px solid black'}, children = subject) for subject in d.index]),
-                                    html.Tr(className = 'scores', children=
-                                        [html.Td(className = 'score', style ={'text-align': 'center', 'border': '1px solid black'}, children = score) for score in d.values])
-                                        ])
+                            util.bang_diem(sbd),
                                     ] + all_graphs
             )
--- util/func.py
+++ util/func.py
@@ -18,7 +18,7 @@ def percentile_all_subjects(data: pd.DataFrame, diem, id: str):
     score = tra_diem(data, diem,id)
     for subject in score.index:
         if np.isnan(score[subject]): continue
-        print(f'Môn {subject}: {percentile(diem[subject], score[subject])}')
+        return f'Môn {subject}: {percentile(diem[subject], score[subject])}'
 
 def higher(data: pd.DataFrame, value):
     'Return the percent of value that is SMALLER than given value'
@@ -34,7 +34,10 @@ def custom_combi(valid: dict, sbd = None):
     custom_data = sum([(diem[subjects[subjects_lower.index(val)]] * valid[val]) for val in valid.keys()])
     data_to_plot = custom_data.value_counts().sort_index()
     title = ", ".join([f'{subjects[subjects_lower.index(val)]} (hệ số {valid[val]})' for val in valid.keys()])
-    fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, template="ggplot2", title=(f'Điểm thi môn {title}'))
+    if "van" in valid.keys():
+        fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, template="ggplot2", title=(f'Điểm thi môn {title}'))
+    else:
+        fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, template="ggplot2", title=(f'Điểm thi môn {title}'))
     fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
     if sbd:
         d = tra_diem(data, diem, sbd)
@@ -68,3 +71,65 @@ def remove_redundant_queries(query_str):
             except ValueError:
                 pass
     return valid
+
+def create_graphs(sbd = 0):
+    if sbd:
+        d = tra_diem(data, diem, sbd)
+        personal_sj = diem[d.index] # Remove NaN scores
+
+    # Create graphs for every subjects
+    all_graphs = []
+    if sbd:
+        for col in personal_sj.columns:
+            if col == "Văn":
+                data_to_plot = personal_sj[col].value_counts().sort_index()
+                fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
+                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
+            else:
+                data_to_plot = personal_sj[col].value_counts().sort_index()
+                fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
+                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
+            info = html.Div(className='info', children = 
+                    [html.H6(f'Điểm của bạn: {d[col]:.2f}'),
+                    html.H6(f'''Điểm của bạn cao hơn {percentile(data=diem[col], value = d[col])} các thí sinh khác! Chỉ có {higher(data=diem[col], value = d[col])} thí sinh có điểm cao hơn bạn!'''),
+                    ])
+            all_graphs.append(
+                html.Div(className = 'fig_container' ,children=[
+                    dcc.Graph(figure=fig, className='graph'),
+                    info,
+                    ]))
+    else:
+        for col in diem.columns:
+            if col == "Văn":
+                data_to_plot = diem[col].value_counts().sort_index()
+                fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
+                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
+            else:
+                data_to_plot = diem[col].value_counts().sort_index()
+                fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
+                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
+
+            info = html.Div(className='info', children = 
+                        [html.H6(f'Điểm trung bình: {diem[col].mean():.2f}'),
+                        html.H6(f'Số điểm nhiều thí sinh đạt được nhất: {diem[col].mode().iloc[0]}'),
+                        html.H6(f'Độ lệch chuẩn: {diem[col].std():.2f}'),
+                        ])
+
+            all_graphs.append(
+                html.Div(className = 'fig_container' ,children=[
+                    dcc.Graph(figure=fig, className='graph'),
+                    info,
+                    ]))
+
+    return all_graphs
+
+def bang_diem(sbd):
+    d = tra_diem(data, diem, sbd)
+    return html.Table(
+        children=[
+            html.Tr(className='subjects', children=
+                [html.Th(className = 'subject', style={'text-align': 'center', 'border': '1px solid black'}, children = subject) for subject in d.index]),
+            html.Tr(className = 'scores', children=
+                [html.Td(className = 'score', style ={'text-align': 'center', 'border': '1px solid black'}, children = score) for score in d.values])
+                ])
+
