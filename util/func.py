003651396be41ea5c18de4f3ebc4d7296fc559bc
nmttuan <nmttuan17@gmail.com>
2022-07-18 21:35:57 +0700

    Split functions into smaller ones

--- util/func.py
+++ util/func.py
@@ -8,7 +8,7 @@ from dash import html, dcc
 import plotly.graph_objects as go
 
 
-def tra_diem(data, diem, sbd: str):
+def tra_diem(sbd: str, data = data, diem = diem):
     return diem.loc[data['SBD'] == int(sbd)].iloc[0].dropna()
 
 def percentile(data: pd.DataFrame, value):
@@ -17,7 +17,7 @@ def percentile(data: pd.DataFrame, value):
     return f'{temp:.2%}'
 
 def percentile_all_subjects(data: pd.DataFrame, diem, id: str):
-    score = tra_diem(data, diem,id)
+    score = tra_diem(id)
     for subject in score.index:
         if np.isnan(score[subject]): continue
         return f'Môn {subject}: {percentile(diem[subject], score[subject])}'
@@ -42,7 +42,7 @@ def custom_combi(valid: dict, sbd = None):
         fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, template="ggplot2", title=(f'Điểm thi môn {title}'))
     fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
     if sbd:
-        d = tra_diem(data, diem, sbd)
+        d = tra_diem(sbd)
         custom_d = sum([(d[subjects[subjects_lower.index(val)]] * valid[val]) for val in valid.keys()])
 
         info = html.Div(className='info', children = 
@@ -74,59 +74,49 @@ def remove_redundant_queries(query_str):
                 pass
     return valid
 
-def create_graphs(sbd = 0):
+def create_graph(sbd = 0, mon = '', graph_type = 'bar', df_diem = diem):
     if sbd:
-        d = tra_diem(data, diem, sbd)
-        personal_sj = diem[d.index] # Remove NaN scores
+        d = tra_diem(sbd = sbd)
+        personal_sj = df_diem[d.index] # Remove NaN scores
 
-    # Create graphs for every subjects
-    all_graphs = []
-    if sbd:
-        for col in personal_sj.columns:
-            if col == "Văn":
-                data_to_plot = personal_sj[col].value_counts().sort_index()
-                fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
-                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
-            else:
-                data_to_plot = personal_sj[col].value_counts().sort_index()
-                fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
-                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
-            info = html.Div(className='info', children = 
-                    [html.H6(f'Điểm của bạn: {d[col]:.2f}'),
-                    html.H6(f'''Điểm của bạn cao hơn {percentile(data=diem[col], value = d[col])} các thí sinh khác! Chỉ có {higher(data=diem[col], value = d[col])} thí sinh có điểm cao hơn bạn!'''),
-                    ])
-            all_graphs.append(
-                html.Div(className = 'fig_container' ,children=[
-                    dcc.Graph(figure=fig, className='graph'),
-                    info,
-                    ]))
+        if not mon and mon not in personal_sj:
+            raise KeyError(f'Thí sinh có số báo danh {sbd} không thi môn {mon}')
+    
+        data_to_plot = personal_sj[mon].value_counts().sort_index()
+    
     else:
-        for col in diem.columns:
-            if col == "Văn":
-                data_to_plot = diem[col].value_counts().sort_index()
-                fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
-                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
-            else:
-                data_to_plot = diem[col].value_counts().sort_index()
-                fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {col}', template="ggplot2")
-                fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
-
-            info = html.Div(className='info', children = 
-                        [html.H6(f'Điểm trung bình: {diem[col].mean():.2f}'),
-                        html.H6(f'Số điểm nhiều thí sinh đạt được nhất: {diem[col].mode().iloc[0]}'),
-                        html.H6(f'Độ lệch chuẩn: {diem[col].std():.2f}'),
+        data_to_plot = df_diem[mon].value_counts().sort_index()
+    
+    if graph_type == 'line':
+        fig = px.line(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {mon}', template="ggplot2")
+    else:
+        fig = px.bar(x = data_to_plot.index.sort_values(), y = data_to_plot, title=f'Điểm thi môn {mon}', template="ggplot2")
+    
+    fig.update_layout(xaxis_title='Điểm', yaxis_title='Số thí sinh')
+    
+    if sbd:
+        info = html.Div(className='info', children = 
+                    [html.H6(f'Điểm của bạn: {d[mon]:.2f}'),
+                    html.H6(f'''Điểm của bạn cao hơn {percentile(data=df_diem[mon], value = d[mon])} các thí sinh khác! Chỉ có {higher(data=df_diem[mon], value = d[mon])} thí sinh có điểm cao hơn bạn!'''),
+                    ])
+        
+    else:
+        info = html.Div(className='info',
+                        children = [
+                            html.H6(f'Điểm trung bình: {df_diem[mon].mean():.2f}'),
+                            html.H6(f'Số điểm nhiều thí sinh đạt được nhất: {diem[mon].mode().iloc[0]}'),
+                            html.H6(f'Độ lệch chuẩn: {df_diem[mon].std():.2f}'),
                         ])
 
-            all_graphs.append(
-                html.Div(className = 'fig_container' ,children=[
-                    dcc.Graph(figure=fig, className='graph'),
-                    info,
-                    ]))
 
-    return all_graphs
+    return (html.Div(className = 'fig_container',
+                    children=[
+                        dcc.Graph(figure=fig, className='graph'),
+                        info,
+                        ]))
 
 def bang_diem(sbd):
-    d = tra_diem(data, diem, sbd)
+    d = tra_diem(sbd)
     return html.Table(
         children=[
             html.Tr(className='subjects', children=
@@ -151,11 +141,15 @@ def count_cao_hon_hoac_bang(muc_diem, mon, df_diem = diem, percent = False):
         return np.nan_to_num(res/custom_data.count()) * 100
     return res
 
-def choropleth_map(mon, muc_diem, percent = False):
+def choropleth_map(mon, muc_diem, percent = False, region = None):
+
+    # Load geojson file. Just don't touch it
     import json
     with open(r'data/diaphantinh.geojson', encoding='utf8') as f:
         map_ = json.load(f)
+
     tinh = pd.DataFrame([[tinh['properties']["id"], tinh['properties']["ten_tinh"]] for tinh in map_['features']])
+
     diem_moi_tinh = pd.concat([tinh, tinh[0].map(lambda x: count_cao_hon_hoac_bang(muc_diem = muc_diem, mon = mon, df_diem = diem_theo_tinh(x), percent=percent))], axis = 1)
     diem_moi_tinh.columns = ['id', 'name', 'value']
 
@@ -174,10 +168,7 @@ def choropleth_map(mon, muc_diem, percent = False):
         sjs = ", ".join([f'{subjects[subjects_lower.index(val)]} (hệ số {mon[val]})' for val in mon.keys()])
     fig.update_layout(title_text=f'{"Số lượng" if not percent else "Tỉ lệ"} thí sinh đạt mức điểm cao hơn hoặc bằng {muc_diem} trên cả nước {f"trong tổ hợp {sjs}" if isinstance(mon, dict) else f"ở môn {subjects[subjects_lower.index(mon)]}"}')
 
-    fig.show()
 
     return html.Div(className='choropleth-container',
                     children=[
                         dcc.Graph(figure=fig)])
-
-choropleth_map(mon = {'toan': 2, 'ly': 1, 'ngoai ngu': 1}, muc_diem=36, percent=True)
